a8155d6dd0d78195b82b91cf4917be48943da58d
Tydien <98918334+Tydi3n@users.noreply.github.com>
2026-06-19 13:17:16 +0200

    Delete quiz2 directory

--- quiz2/views/errors/403.php
+++
@@ -1,5 +0,0 @@
-<section class="empty-state">
-    <h1>Brak dostepu</h1>
-    <p>Ta czesc aplikacji jest dostepna tylko dla konta z odpowiednimi uprawnieniami.</p>
-    <a class="button" href="<?= e(route_url('quizzes.index')) ?>">Wroc do quizow</a>
-</section>
