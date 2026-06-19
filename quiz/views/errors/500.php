78b18ad7d632564813c287042b12655078b0c70f
Tydien <98918334+Tydi3n@users.noreply.github.com>
2026-06-19 13:17:26 +0200

    Delete quiz directory

--- quiz/views/errors/500.php
+++
@@ -1,6 +0,0 @@
-<section class="empty-state">
-    <h1>Blad aplikacji</h1>
-    <p>Wystapil problem podczas obslugi zadania.</p>
-    <p class="muted"><?= e($message ?? 'Brak szczegolow bledu.') ?></p>
-    <a class="button" href="<?= e(route_url('quizzes.index')) ?>">Wroc do quizow</a>
-</section>
